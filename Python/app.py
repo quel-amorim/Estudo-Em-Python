f9ccce7d701ce1e0dc1427700ae0ec300eacdbc3
Amorim <ezequielssamorim@gmail.com>
2025-12-01 16:11:10 -0300

    Add menu option for Categories in app.py

--- Python/app.py
+++ Python/app.py
@@ -2,6 +2,7 @@ from models.CriacaoCalculadora import Calculadora
 from models.CriacaoTabuada import Tabuada
 from models.ProdutosCriados import Produto
 from models.Curriculo import Perfil
+from models.CriacaoMenuCafe import Categoria 
 print('-'*50)
 print('Bem-vindo ao Menu de Programas já feitos em Python')
 print('-'*50)
@@ -10,12 +11,15 @@ print('[1] Calculadora')
 print('[2] Tabuada')
 print('[3] Programa de Produtos')
 print('[4] Criar curriculo')
+print('[5] Catégorias')
 p = int(input('Vamos ver o ... :'))
 if p == 1:
-    Calculadora().rodar_calculadora()
+    Calculadora().menu()
 elif p == 2:
     Tabuada().rodar_tabuada()
 elif p == 3:
     Produto().menu_produtos()
 elif p == 4:
     Perfil().menu_perfil()
+elif p == 5:
+    Categoria().menu()
