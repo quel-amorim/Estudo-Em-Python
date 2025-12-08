20fdd193768d937577517e6c0e5433662d4b8acc
Amorim <ezequielssamorim@gmail.com>
2025-12-08 13:27:04 -0300

    Add new options for tasks and guessing game

--- Python/app.py
+++ Python/app.py
@@ -2,7 +2,9 @@ from models.CriacaoCalculadora import Calculadora
 from models.CriacaoTabuada import Tabuada
 from models.ProdutosCriados import Produto
 from models.Curriculo import Perfil
-from models.CriacaoMenuCafe import Categoria 
+from models.CriacaoMenuCafe import Categoria
+from models.Tarefas import Tarefa
+from models.Jogo_Adivinha import Adivinha 
 print('-'*50)
 print('Bem-vindo ao Menu de Programas já feitos em Python')
 print('-'*50)
@@ -12,6 +14,8 @@ print('[2] Tabuada')
 print('[3] Programa de Produtos')
 print('[4] Criar curriculo')
 print('[5] Catégorias')
+print('[6] Tarefas')
+print('[7] Jogo de Adivinhar')
 p = int(input('Vamos ver o ... :'))
 if p == 1:
     Calculadora().menu()
@@ -23,3 +27,7 @@ elif p == 4:
     Perfil().menu_perfil()
 elif p == 5:
     Categoria().menu()
+elif p == 6:
+    Tarefa().menu()
+elif p == 7:
+    Adivinha().MenuPrograma()
