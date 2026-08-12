54dc214d0b0f30193be5d5ce15927a24f3327eca
quel-Amorim <ezequielssamorim@gmail.com>
2026-08-12 17:24:57 -0300

    arrumando variaveis do gerenciamentoProdutos.py

--- src/praticas/gerenciarContas.py
+++ src/praticas/gerenciarContas.py
@@ -2,23 +2,25 @@
 #Versão 1 POO - Criação da classe conta e gerenciar
 #Fazer : melhor o CRUD(criar,exibir,remover,editar) , fazer uma classe pessoa tendo as informações de nome,email e salario
 from random import uniform
+from datetime import timedelta,datetime
 class Conta:
-    def __init__(self,nomeConta,valor,dataVencimento):
-        self.nomeConta = nomeConta
+    def __init__(self,nomeconta,valor):
+        self.nomeconta = nomeconta
         self.valor = valor
-        self.dataVencimento = dataVencimento
+        self.dataatual = datetime.now()
+        self.vencimento = timedelta(days=15) + self.dataatual
 
-luz = Conta('Conta de LUZ',178.99,'14/07/2026')
-agua = Conta('Conta de Agua',159.89,'10/06/2026')
+luz = Conta('Conta de LUZ',178.99)
+agua = Conta('Conta de Agua',159.89)
 
 class Gerenciar:
     def __init__(self):
         self.gerenciamento = [luz,agua]
-        self.salarioPadrao = round(uniform(1500,3000),2)
+        self.salariopadrao = round(uniform(1500,3000),2)
 
     def exibir(self):
         for x in self.gerenciamento:
-            print('-' , x.nomeConta ,'R$ :', x.valor ,'Vencimento',x.dataVencimento)
+            print('-' , x.nomeconta ,'R$ :', x.valor ,'Fechamento de Fatura',x.dataatual.strftime('%d/%m/%Y'),'Vencimento',x.vencimento.strftime('%d/%m/%Y'))
 
 def main():
     admin = Gerenciar()
