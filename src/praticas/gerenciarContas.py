b7e1f20d09278b9baa8af06686c2b6a271b39b5c
quel-Amorim <ezequielssamorim@gmail.com>
2026-08-12 15:54:47 -0300

    definações de que fazer gerenciarContas.py

---
+++ src/praticas/gerenciarContas.py
@@ -0,0 +1,30 @@
+'''Projeto de Listamento de contas'''
+#Versão 1 POO - Criação da classe conta e gerenciar
+#Fazer : melhor o CRUD(criar,exibir,remover,editar) , fazer uma classe pessoa tendo as informações de nome,email e salario
+from random import uniform
+class Conta:
+    def __init__(self,nomeConta,valor,dataVencimento):
+        self.nomeConta = nomeConta
+        self.valor = valor
+        self.dataVencimento = dataVencimento
+
+
+luz = Conta('Conta de LUZ',178.99,'14/07/2026')
+agua = Conta('Conta de Agua',159.89,'10/06/2026')
+
+class Gerenciar:
+    def __init__(self):
+        self.gerenciamento = [luz,agua]
+        self.salarioPadrao = round(uniform(1500,3000),2)
+
+    def exibir(self):
+        for x in self.gerenciamento:
+            print('-' , x.nomeConta ,'R$ :', x.valor ,'Vencimento',x.dataVencimento)
+
+def main():
+    admin = Gerenciar()
+
+    admin.exibir()
+
+if __name__ == '__main__':
+    main()
